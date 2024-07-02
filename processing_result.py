f5dd661f748e17689bfd50a5766d9b846d736663
Eren Yilmaz <erenyilmaz95@gmx.de>
2024-07-02 12:36:51 +0200

    Draft some more image processing results

--- processing_result.py
+++ processing_result.py
@@ -2,7 +2,7 @@ from typing import Dict, Any, List
 
 
 class ProcessingResult:
-    def __init__(self, tool_name: str, metadata: Dict[str, Any]=None):
+    def __init__(self, tool_name: str, metadata: Dict[str, Any] = None):
         self.tool_name = tool_name
         if metadata is None:
             metadata = {}
@@ -10,3 +10,16 @@ class ProcessingResult:
 
     def to_dicom(self) -> List[str]:
         raise NotImplementedError('Abstract method')
+
+
+class SegmentationResult(ProcessingResult):
+    def to_dicom(self) -> List[str]:
+        raise NotImplementedError('TO DO')
+
+
+class PDFResult(ProcessingResult):
+    def __init__(self, tool_name: str, pdf_path: str, metadata: Dict[str, Any] = None):
+        self.pdf_path = pdf_path
+
+    def to_dicom(self) -> List[str]:
+        raise NotImplementedError('TO DO')
