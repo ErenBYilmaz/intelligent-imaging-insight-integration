3598a59e55f27e998d040f280331d24e0ac034bb
naymecliff <74153221+naymecliff@users.noreply.github.com>
2024-07-02 17:12:17 +0200

    Update aibutton.py to submit url to study archive

--- orthanc-manipulation/aibutton.py
+++ orthanc-manipulation/aibutton.py
@@ -13,6 +13,7 @@ print('Hello world!')
 
 def ExecutePython(output, uri, **request):
     s = 'Python version: %s' % platform.python_version()
+    print(request.__getitem__("body"))
     output.AnswerBuffer(s, 'text/plain')
 
 orthanc.RegisterRestCallback('/execute-python', ExecutePython)
@@ -33,9 +34,38 @@ $( document ).ready(function() {
         .text('Execute AI Assist')
         .button()
         .click(function(e) {
-            $.get('../execute-python', function(answer) {
-            alert('Das hat alles wunderbar geklappt! YAY!');
-            });
+            let copyButton = document.getElementById("study-archive-link")
+            if(copyButton!==undefined){
+                copyButton.click()
+                setTimeout(() => {
+                    navigator.clipboard.read().then((clipboardContents)=>{
+                        for (const item of clipboardContents) {
+                            if (!item.types.includes("text/plain")) {
+                                throw new Error("Clipboard does not contain PNG image data.");
+                            }
+                            item.getType("text/plain").then(blob => {
+                                console.log(blob)
+                              blob.text().then(url => {
+                                $.post('../execute-python', url, 
+                              
+                                    function(answer) {
+                                        /*put your code after python here*/
+                                        alert('Das hat alles wunderbar geklappt! YAY!');
+                                    });
+                              
+                                
+                              })
+                            });
+                        }      
+                           
+                            
+                    
+                     
+                    });        
+                },100)
+                
+            }
+            
         });
 
         b.insertAfter($('#stow-study'));                      
