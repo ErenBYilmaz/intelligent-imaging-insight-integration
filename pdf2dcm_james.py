f7d30b101e5d8a34df75975728ab7a4b2f41f487
Eren Yilmaz <erenyilmaz95@gmx.de>
2024-07-02 17:17:32 +0200

    Try to get PDF processing running

--- pdf2dcm_james.py
+++ pdf2dcm_james.py
@@ -1,16 +1,12 @@
-!pip install pdf2dcm
-
 from pdf2dcm import Pdf2EncapsDCM
 
-class processing_pdf2dcm():
+
+class PDF2DCMProcessor():
 
     def __init__(self):
-
         self.converter = Pdf2EncapsDCM()
 
-
     def fit(self, pdf_file_path, dcm_template_file_path):
-
         """
         path_pdf (str): path of the pdf that needs to be converted
         path_template_dcm (str, optional): path to template for getting the repersonalisation of data.
@@ -18,19 +14,7 @@ class processing_pdf2dcm():
         """
 
         return self.converter.run(
-                path_pdf= pdf_file_path, 
-                path_template_dcm= dcm_template_file_path, 
-                suffix =".dcm"
-                )
-
-
-
-
-
-
-
-
-
-
-
-
+            path_pdf=pdf_file_path,
+            path_template_dcm=dcm_template_file_path,
+            suffix=".dcm"
+        )
--- processing_result.py
+++ processing_result.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any, List
 
+from pdf2dcm_james import PDF2DCMProcessor
+
 
 class ProcessingResult:
     def __init__(self, tool_name: str, metadata: Dict[str, Any] = None):
@@ -18,9 +20,13 @@ class SegmentationResult(ProcessingResult):
 
 
 class PDFResult(ProcessingResult):
-    def __init__(self, tool_name: str, pdf_path: str, metadata: Dict[str, Any] = None):
+    def __init__(self,
+                 tool_name: str,
+                 pdf_path: str,
+                 metadata: Dict[str, Any] = None):
         super().__init__(tool_name, metadata)
         self.pdf_path = pdf_path
 
     def to_dicom(self) -> List[str]:
-        raise NotImplementedError('TO DO')
+        proc = PDF2DCMProcessor()
+        return proc.fit(pdf_file_path=self.pdf_path, dcm_template_file_path=...)
