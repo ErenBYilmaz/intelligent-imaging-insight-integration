36dc4108ea65636c8081b33c83bd1dd9239266c6
naymecliff <74153221+naymecliff@users.noreply.github.com>
2024-07-03 12:57:51 +0200

    Update aibutton.js to current state

--- orthanc-manipulation/aibutton.js
+++ orthanc-manipulation/aibutton.js
@@ -1,54 +1,61 @@
 $( document ).ready(function() {
-    setInterval(() => {
-        if(!document.getElementById('sample-python-button') && document.getElementById('stow-study')){
-            $('#sample-python-button').remove();                              
-            var b = $('<a>')
-            .attr('id', 'sample-python-button')
-            .attr('data-role', 'button')
-            .attr('href', '#')
-            .attr('data-icon', 'forward')
-            .attr('data-theme', 'a')
-            .text('Execute AI Assist')
-            .button()
-            .click(function(e) {
-                let copyButton = document.getElementById("study-archive-link")
-                if(copyButton!==undefined){
-                    copyButton.click()
-                    setTimeout(() => {
-                        navigator.clipboard.read().then((clipboardContents)=>{
-                            for (const item of clipboardContents) {
-                                if (!item.types.includes("text/plain")) {
-                                    throw new Error("Clipboard does not contain PNG image data.");
-                                }
-                                item.getType("text/plain").then(blob => {
-                                    console.log(blob)
-                                blob.text().then(url => {
-                                    $.post('../execute-python', url, 
+  setInterval(() => {
+    if(!document.getElementById('executeAiAssist') && document.getElementById('stow-study')){
+        $('#executeAiAssist').remove();                              
+        var b = $('<a>')
+        .attr('id', 'executeAiAssist')
+        .attr('data-role', 'button')
+        .attr('href', '#')
+        .attr('data-icon', 'forward')
+        .attr('data-theme', 'a')
+        .text('Execute AI Assist')
+        .button()
+        .click(function(e) {
+            let copyButton = document.getElementById("study-archive-link")
+            if(copyButton!==undefined){
+                copyButton.click()
+                setTimeout(() => {
+                    navigator.clipboard.read().then((clipboardContents)=>{
+                        for (const item of clipboardContents) {
+                            if (!item.types.includes("text/plain")) {
+                                throw new Error("Clipboard does not contain PNG image data.");
+                            }
+                            item.getType("text/plain").then(blob => {
+                                console.log(blob)
+                              blob.text().then(url => {
+                                $.post('../execute-python', url, 
+                              
+                                    function(answer) {
+                                        /*put your after python code here*/
+                                        window.open("C:\Users\info\PycharmProjects\DentalMaskRcnn\results\example_tooth.png");
+                                        alert('Das hat alles wunderbar geklappt! YAY!');
+                                    });
+                              
                                 
-                                        function(answer) {
-                                            /*put your code after python here*/
-                                            alert('Das hat alles wunderbar geklappt! YAY!');
-                                        });
-                                
-                                    
-                                })
-                                });
-                            }      
+                              })
+                            });
+                        }      
+                           
                             
-                                
-                        
-                        
-                        });        
-                    },100)
                     
-                }
+                     
+                    });        
+                },100)
                 
-            });
-
-            b.insertAfter($('#stow-study'));                      
-        } 
-                                                    
-                                
-    }, 200) ;                           
-    
-    }); 
+            }
+            
+        });
+        $(document).keydown(function(event) {
+            if (event.altKey && event.which === 88)
+            {
+                b.click()
+                e.preventDefault();
+            }
+        });
+        b.insertAfter($('#stow-study'));                      
+    } 
+                                                 
+                            
+  }, 200) ;                           
+  
+});          
